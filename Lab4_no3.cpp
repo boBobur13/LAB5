4eec33b7719176644d16410516d144b4e455e5ce
Brain17 <b.daminov@newuu.uz>
2025-06-13 15:41:08 +0500

    Update and rename Lab4_no3.txt to Lab4_no3.cpp

--- Lab4_no3.cpp
+++ Lab4_no3.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int n;
-    double sum = 0;
+    double sum= 0;
     cout << "enter a number  ";
     cin >> n;
-    for (int i = 1;i<= n; i++) {
+    for (int i = 1;i<=n; i++) {
         cout<< "1/" << i;
         if(i != n) {
             cout << " + ";
